96838085cc2f90138ce3735f8ae1309b04e64cac
Denny09310 <k.denny2000@gmail.com>
2024-09-16 17:08:37 +0200

    feat: complete ion-back-button, add ion-buttons

--- Blazor.Ionic.Components/IonBackButton.cs
+++ Blazor.Ionic.Components/IonBackButton.cs
@@ -6,7 +6,25 @@ namespace Blazor.Ionic.Components;
 public partial class IonBackButton : IonComponentBase
 {
     [Parameter]
+    public string? Color { get; set; }
+
+    [Parameter]
     public string? DefaultHref { get; set; }
+
+    [Parameter]
+    public bool? Disabled { get; set; }
+
+    [Parameter]
+    public string? Icon { get; set; }
+
+    [Parameter]
+    public string? Mode { get; set; }
+
+    [Parameter]
+    public string? Text { get; set; }
+
+    [Parameter]
+    public string? Type { get; set; }
 
     protected override void BuildRenderTree(RenderTreeBuilder builder)
     {
@@ -14,7 +32,13 @@ public partial class IonBackButton : IonComponentBase
 
         builder.OpenElement(i++, "ion-back-button");
 
+        builder.AddAttribute(i++, "color", Color);
         builder.AddAttribute(i++, "default-href", DefaultHref);
+        builder.AddAttribute(i++, "disabled", ToAttribute(Disabled));
+        builder.AddAttribute(i++, "icon", Icon);
+        builder.AddAttribute(i++, "mode", Mode);
+        builder.AddAttribute(i++, "text", Text);
+        builder.AddAttribute(i++, "type", Type);
         builder.AddAttribute(i++, "onclick", "history.back()");
 
         builder.AddMultipleAttributes(i++, AdditionalAttributes);
---
+++ Blazor.Ionic.Components/IonBackButtons.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Components.Rendering;
+using Microsoft.AspNetCore.Components;
+
+namespace Blazor.Ionic.Components;
+
+public partial class IonButtons : IonComponentBase
+{
+    [Parameter]
+    public bool? Collapse { get; set; }
+
+    protected override void BuildRenderTree(RenderTreeBuilder builder)
+    {
+        int i = 0;
+
+        builder.OpenElement(i++, "ion-buttons");
+
+        builder.AddAttribute(i++, "collapse", ToAttribute(Collapse));
+
+        builder.AddMultipleAttributes(i++, AdditionalAttributes);
+
+        builder.AddContent(i, ChildContent);
+
+        builder.CloseElement();
+
+        base.BuildRenderTree(builder);
+    }
+}
